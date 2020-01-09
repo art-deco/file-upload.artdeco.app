51a725a77ed9d34b8e2e93d18adb400fcb04bc31
Anton <anton@adc.sh>
2020-01-09 19:35:23 +0300

    links

--- routes/get/1index.jsx
+++ routes/get/1index.jsx
@@ -5,6 +5,7 @@
 export default async function index(ctx, next) {
   ctx.body = ctx.render([<div>
     <h1>Idio File Upload Example</h1>
+    <p>This is a demo of <em>Idio</em> web server - a Koa server will essential middleware compiled with Closure Compiler to rely only on 2 dependencies. See <a target="_blank" noreferrer href="https://github.com/art-deco/file-upload.artdeco.app">GitHub repository</a> for the full source code and more explanation how this app works, and the <a target="_blank" noreferrer href="https://github.com/idiocc/idio">Idio page</a> to learn more about this Node.JS server.</p>
   </div>,
   <div id="preact-container"></div>,
   ctx.PROD ? <script>{`window.HOST='${ctx.HOST}'`}</script> : null,
